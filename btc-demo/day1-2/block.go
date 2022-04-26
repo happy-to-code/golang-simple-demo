fd081a0277b81b8cdc9ef25e1e6eb1a1102c8344
happy-to-code <yida8080@gmail.comc>
2022-04-26 23:00:52 +0800

    block 增加字段

--- btc-demo/day1-2/block.go
+++ btc-demo/day1-2/block.go
@@ -1,25 +1,51 @@
 package main
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"time"
+)
 
 type Block struct {
-	PreHash []byte
-	Hash    []byte
-	Data    []byte
+	Version    uint64
+	PrevHash   []byte
+	MerkelRoot []byte
+	TimeStamp  uint64
+	Difficulty uint64
+	Nonce      uint64
+	Hash       []byte
+	Data       []byte
+}
+
+// Uint642Byte 将uint64 ==>[]byte
+func Uint642Byte(i uint64) (by []byte) {
+	return
 }
 
 func NewBlock(data string, preBlockHash []byte) *Block {
 	block := Block{
-		PreHash: preBlockHash,
-		Hash:    []byte{},
-		Data:    []byte(data),
+		Version:    00,
+		PrevHash:   preBlockHash,
+		MerkelRoot: []byte{},
+		TimeStamp:  uint64(time.Now().Unix()),
+		Difficulty: 0,
+		Nonce:      0,
+		Hash:       []byte{},
+		Data:       []byte(data),
 	}
 	// 设置hash值
 	block.SetHash()
 	return &block
 }
 func (block *Block) SetHash() {
-	blockInfo := append(block.PreHash, block.Data...)
+	var blockInfo []byte
+	blockInfo = append(blockInfo, Uint642Byte(block.Version)...)
+	blockInfo = append(blockInfo, block.PrevHash...)
+	blockInfo = append(blockInfo, block.MerkelRoot...)
+	blockInfo = append(blockInfo, Uint642Byte(block.TimeStamp)...)
+	blockInfo = append(blockInfo, Uint642Byte(block.Difficulty)...)
+	blockInfo = append(blockInfo, Uint642Byte(block.Nonce)...)
+	blockInfo = append(blockInfo, block.Data...)
+
 	hash := sha256.Sum256(blockInfo)
 	block.Hash = hash[:]
 }
--- btc-demo/day1-2/btc.go
+++ btc-demo/day1-2/btc.go
@@ -10,7 +10,7 @@ func main() {
 	bc.AddBlock("高度为2的区块")
 	for i, block := range bc.blocks {
 		fmt.Printf("===============================当前区块高度：%d=============================\n", i)
-		fmt.Printf("前区块哈希值：%x\n", block.PreHash)
+		fmt.Printf("前区块哈希值：%x\n", block.PrevHash)
 		fmt.Printf("当前区块哈希值：%x\n", block.Hash)
 		fmt.Printf("区块数据：%s\n", block.Data)
 	}
