828dfa92b7a2b58cb892c8b515d89721ca4f98c0
happy-to-code <yida8080@gmail.comc>
2022-04-26 22:29:48 +0800

    btc  blockchain

--- btc-demo/day1/btc.go
+++ btc-demo/day1/btc.go
@@ -42,13 +42,22 @@ func GenesisBlock() *Block {
 	return NewBlock("我是创世区块", []byte{})
 }
 
+func (bc *BlockChain) AddBlock(data string) {
+	lastBlock := bc.blocks[len(bc.blocks)-1]
+	preHash := lastBlock.Hash
+
+	block := NewBlock(data, preHash)
+	bc.blocks = append(bc.blocks, block)
+}
+
 func main() {
 	bc := NewBlockChain()
+	bc.AddBlock("高度为1的区块")
+	bc.AddBlock("高度为2的区块")
 	for i, block := range bc.blocks {
 		fmt.Printf("===============================当前区块高度：%d=============================\n", i)
 		fmt.Printf("前区块哈希值：%x\n", block.PreHash)
 		fmt.Printf("当前区块哈希值：%x\n", block.Hash)
 		fmt.Printf("区块数据：%s\n", block.Data)
 	}
-
 }
