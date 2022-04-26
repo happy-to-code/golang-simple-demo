e43bc2c3aec3d10d3d2d77a84205268dfc2ff8ff
happy-to-code <yida8080@gmail.comc>
2022-04-26 22:22:46 +0800

    btc  blockchain

--- btc-demo/day1/btc.go
+++ btc-demo/day1/btc.go
@@ -27,9 +27,28 @@ func (block *Block) SetHash() {
 	block.Hash = hash[:]
 }
 
+type BlockChain struct {
+	blocks []*Block
+}
+
+func NewBlockChain() *BlockChain {
+	// 获取创世区块
+	genesisBlock := GenesisBlock()
+	return &BlockChain{
+		blocks: []*Block{genesisBlock},
+	}
+}
+func GenesisBlock() *Block {
+	return NewBlock("我是创世区块", []byte{})
+}
+
 func main() {
-	block := NewBlock("创世区块", []byte{})
-	fmt.Printf("前区块哈希值：%x\n", block.PreHash)
-	fmt.Printf("当前区块哈希值：%x\n", block.Hash)
-	fmt.Printf("区块数据：%s\n", block.Data)
+	bc := NewBlockChain()
+	for i, block := range bc.blocks {
+		fmt.Printf("===============================当前区块高度：%d=============================\n", i)
+		fmt.Printf("前区块哈希值：%x\n", block.PreHash)
+		fmt.Printf("当前区块哈希值：%x\n", block.Hash)
+		fmt.Printf("区块数据：%s\n", block.Data)
+	}
+
 }
